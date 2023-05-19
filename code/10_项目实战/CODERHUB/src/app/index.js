1f8d81830cbad32509a865c0100f02eb6e0c77dc
liuyihua <529732278@qq.com>
2023-05-19 23:12:33 +0800

    自动注册路由

--- code/10_项目实战/CODERHUB/src/app/index.js
+++ code/10_项目实战/CODERHUB/src/app/index.js
@@ -1,8 +1,9 @@
 //1.导入koa
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const userRouter = require('../router/user.router');
-const loginRouter = require('../router/login.router');
+const registerRoutes = require('../router');
+// const userRouter = require('../router/user.router');
+// const loginRouter = require('../router/login.router');
 
 //创建app对象
 const app = new koa();
@@ -10,9 +11,6 @@ const app = new koa();
 //注册中间件
 app.use(bodyParser());
 
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
-app.use(loginRouter.routes());
-app.use(loginRouter.allowedMethods());
+registerRoutes.call(app);
 
 module.exports = app;
